9f542b2a7db95beda1151999492e8485dd5d3e7a
midCenturyDude7 <mgriffes@gmail.com>
2022-10-23 15:49:02 -0700

    Created counter function via event listener

--- 1. Build a Passenger Counter App/12. Write a function that increments/index.js
+++ 1. Build a Passenger Counter App/12. Write a function that increments/index.js
@@ -1,9 +1,14 @@
-let lapsCompleted = 0
+let lapsCompleted = 0;
 
 // Create a function that increments the lapsCompleted variable with one
 // Run it three times
 
+function lapCounter() {
+  lapsCompleted += 1;
+  console.log(lapsCompleted);
+}
 
 
-
-console.log(lapsCompleted)
+lapCounter();
+lapCounter();
+lapCounter();
--- 1. Build a Passenger Counter App/13. Increment on clicks/index.js
+++ 1. Build a Passenger Counter App/13. Increment on clicks/index.js
@@ -9,6 +9,16 @@
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
+let count = 0;
+
+function increment() {
+  console.log("Clicked!!");
+  count += 1;
+  console.log(count);
+
+  document.getElementById("count-el").innerText = count;
+}
+
 
 
 
