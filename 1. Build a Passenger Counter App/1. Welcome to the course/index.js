3e5342c7d9a091142446c94f6a8d6b51b2072bc9
midCenturyDude7 <mgriffes@gmail.com>
2022-10-23 15:21:20 -0700

    Working with JS and the DOM

--- 1. Build a Passenger Counter App/1. Welcome to the course/index.js
+++ 1. Build a Passenger Counter App/1. Welcome to the course/index.js
@@ -1,1 +1,18 @@
 // javascript
+// document.getElementById("count-el").innerText = 5;
+
+// let count = 5 + 7;
+// console.log(count);
+
+// Step 1
+let bonusPoints = 50;
+bonusPoints += 50;
+console.log(bonusPoints);
+
+// Step 2
+bonusPoints -= 75;
+console.log(bonusPoints);
+
+// Step 3
+bonusPoints += 45;
+console.log(bonusPoints);
