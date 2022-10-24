0240fe4e2f07b83dc1690b77edcf4948fde34ea9
midCenturyDude7 <mgriffes@gmail.com>
2022-10-24 15:50:23 -0700

    Finished practice challenges

--- 2. Practice time - part 1/5. Rendering an error message/index.js
+++ 2. Practice time - part 1/5. Rendering an error message/index.js
@@ -2,4 +2,9 @@
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
 
+let errorMsg = document.getElementById("error");
 
+function purchase() {
+  console.log("Button clicked!");
+  errorMsg.textContent = "Something went wrong, please try again";
+}
