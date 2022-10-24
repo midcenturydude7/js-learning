fdb69452d56f68d4c9f53d2652bae8e2addcae78
midCenturyDude7 <mgriffes@gmail.com>
2022-10-24 10:08:52 -0700

    Working with strings and concatation

--- 1. Build a Passenger Counter App/19. Write your first string variable/index.js
+++ 1. Build a Passenger Counter App/19. Write your first string variable/index.js
@@ -1,4 +1,6 @@
-let username = 'per"
+let username = "midCenturyDude7";
+let message = "you have three new notifications";
+let messageToUser = "Hey " + username + ", " + message + ".";
 
-console.log(username)
-
+console.log(username);
+console.log(messageToUser);
