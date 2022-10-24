861f7d1da1408f70448da5270f283c53d2d3bb7d
midCenturyDude7 <mgriffes@gmail.com>
2022-10-24 15:06:26 -0700

    Completed train station counter app

--- 1. Build a Passenger Counter App/25. Create the save feature/index.js
+++ 1. Build a Passenger Counter App/25. Create the save feature/index.js
@@ -1,17 +1,23 @@
 // 1. Grab the save-el paragrah and store it in a variable called saveEl
-let countEl = document.getElementById("count-el")
-let count = 0
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+let saveEl = document.getElementById("save-el");
 
 function increment() {
-    count += 1
-    countEl.innerText = count
+  count += 1;
+  countEl.innerText = count;
 }
 
 function save() {
-    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    // 3. Render the variable in the saveEl using innerText
-    // NB: Make sure to not delete the existing content of the paragraph
-    console.log(count)
+  // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+  let dashValue = count + " - ";
+
+  // 3. Render the variable in the saveEl using innerText
+  saveEl.innerText += dashValue;
+
+  // NB: Make sure to not delete the existing content of the paragraph
+  console.log(count);
 }
 
 
