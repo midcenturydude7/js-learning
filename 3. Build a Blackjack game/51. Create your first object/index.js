083e0f7fa3159caf46e08e10ffa828838d1714ca
midCenturyDude7 <mgriffes@gmail.com>
2022-10-26 19:28:16 -0700

    Working with objects

--- 3. Build a Blackjack game/51. Create your first object/index.js
+++ 3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,12 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
+let castle = {
+  location: "Europe",
+  roomRate: 150,
+  available: true,
+  amenities: ["refrigerator", "wifi"]
+};
 
+console.log(castle.location);
+console.log(castle.available);
