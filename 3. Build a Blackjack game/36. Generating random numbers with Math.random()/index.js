ce0a595211658f91575d798b2d76b7598013b2c6
midCenturyDude7 <mgriffes@gmail.com>
2022-10-26 17:20:59 -0700

    Working with random numbers

--- 3. Build a Blackjack game/36. Generating random numbers with Math.random()/index.js
+++ 3. Build a Blackjack game/36. Generating random numbers with Math.random()/index.js
@@ -1,12 +1,12 @@
-let randomNumber = Math.random()
+let randomNumber = Math.random();
 
-console.log(randomNumber)
+console.log(randomNumber);
 
 
 /* 
 
 What does Math.random() do?
 
-Your answer: 
+Your answer: It is a method that returns a numeric value between 0 and 1
 
 */
