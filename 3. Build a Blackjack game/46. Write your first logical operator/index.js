3fd933b1aa01907c3ca62de918d6f8d6b99ba7ae
midCenturyDude7 <mgriffes@gmail.com>
2022-10-26 18:46:40 -0700

    Working with assignment operators

--- 3. Build a Blackjack game/46. Write your first logical operator/index.js
+++ 3. Build a Blackjack game/46. Write your first logical operator/index.js
@@ -1,11 +1,15 @@
-let hasSolvedChallenge = false
-let hasHintsLeft = false
+let hasSolvedChallenge = false;
+let hasHintsLeft = false;
 
 // Create an if statement that checks that both variables are false.
 // If so, run the showSolution() function
 
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
+  showSolution();
+}
+
 function showSolution() {
-    console.log("Showing the solution....")
+  console.log("Showing the solution....");
 }
 
 
