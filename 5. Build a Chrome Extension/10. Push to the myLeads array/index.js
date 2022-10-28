c4767ca7b54fd9de98b7420e8c83b53c3da9d324
midCenturyDude7 <mgriffes@gmail.com>
2022-10-28 16:33:14 -0700

    Working with innerHTML to dynamically render data

--- 5. Build a Chrome Extension/10. Push to the myLeads array/index.js
+++ 5. Build a Chrome Extension/10. Push to the myLeads array/index.js
@@ -1,11 +1,12 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
 
 // Push the value "www.awesomelead.com" to myArray when the input button is clicked
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
-})
+  myLeads.push("www.awesomelead.com");
+  console.log(myLeads);
+});
 
 
