5523b3ddf5f4fc77aae1e2c96d58bcb25db65679
midCenturyDude7 <mgriffes@gmail.com>
2022-10-27 21:40:40 -0700

    Generating a random index number with Math.random()

--- 4. Practice time - part 2/6. Rock papers scissors/index.js
+++ 4. Practice time - part 2/6. Rock papers scissors/index.js
@@ -1,5 +1,9 @@
-let hands = ["rock", "paper", "scissor"]
+let hands = ["rock", "paper", "scissor"];
 
 // Create a function that returns a random item from the array
+function roShamBo() {
+  let randomIndex = Math.floor(Math.random() * 3) +1;
+  return randomIndex;
+}
 
-
+console.log(roShamBo());
