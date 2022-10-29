74f59f4d2207a960f5cceac22ecaa3a94c3d756a
midCenturyDude7 <mgriffes@gmail.com>
2022-10-28 21:48:16 -0700

    Successfully deployed working draft of chrome extension

--- 5. Build a Chrome Extension/16. Write your first innerHTML/index.js
+++ 5. Build a Chrome Extension/16. Write your first innerHTML/index.js
@@ -1,3 +1,4 @@
 // Use .innerHTML to render a Buy! button inside the div container
-
+const buyBtn = document.querySelector("#container");
+buyBtn.innerHTML = "<button>" + "Buy!" + "</button>";
 
--- 5. Build a Chrome Extension/26. Make the template string even more dynamic/index.js
+++ 5. Build a Chrome Extension/26. Make the template string even more dynamic/index.js
@@ -1,9 +1,10 @@
 // template strings/literals
 
-const recipient = "James"
+const recipient = "James";
+const sender = "Matt";
 // Create a new variable, sender, and set its value to your name
 
 // Use your sender variable instead of "Per"
-const email = `Hey ${recipient}! How is it going? Cheers Per`
+const email = `Hey ${sender}! How is it going? Cheers Per`;
 
-console.log(email)
+console.log(email);
