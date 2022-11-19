34b4cd45cf6b01da336e65e803fbffb192a81014
midCenturyDude7 <mgriffes@gmail.com>
2022-11-18 21:42:39 -0800

    Working with .filter() and .include()

--- 8. Meme Picker App/index.js
+++ 8. Meme Picker App/index.js
@@ -1,11 +1,32 @@
 import { catsData } from "./data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
+const getImageBtn = document.getElementById("get-image-btn");
+const gifsOnlyOption = document.getElementById("gifs-only-option");
 
 emotionRadios.addEventListener("change", highlightCheckedOption);
+getImageBtn.addEventListener("click", getMatchingCatsArray);
 
 function highlightCheckedOption(e) {
+  const radioClass = document.getElementsByClassName("radio");
+
+  for (let radioBtn of radioClass) {
+    radioBtn.classList.remove("highlight");
+  }
+
   document.getElementById(e.target.id).parentElement.classList.add("highlight");
+}
+
+function getMatchingCatsArray() {
+  const isGif = gifsOnlyOption.checked;
+  console.log(isGif);
+
+  if (document.querySelector("input[type='radio']:checked")) {
+    const checkedRadioInput = document.querySelector(
+      "input[type='radio']:checked"
+    ).value;
+    console.log(checkedRadioInput);
+  }
 }
 
 function getEmotionsArray(cats) {
