f13c40db6659c78e6c2d880bf1df24fe8b09f18a
midCenturyDude7 <mgriffes@gmail.com>
2022-11-25 15:18:19 -0800

    Working with forEach() to iterate over data

--- 9. Twimba: Twitter Clone/app.js
+++ 9. Twimba: Twitter Clone/app.js
@@ -1,1 +1,39 @@
 import { tweetsData } from "./data.js";
+
+const tweetInput = document.getElementById("tweet-input");
+const tweetBtn = document.getElementById("tweet-btn");
+
+tweetBtn.addEventListener("click", function () {
+  tweetInput.innerHTML = tweetInput.value;
+});
+
+function getFeedHtml() {
+  let feedHtml = "";
+  for (let tweet of tweetsData) {
+    feedHtml += `
+    <div class="tweet">
+      <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+          <p class="handle">${tweet.handle}</p>
+          <p class="tweet-text">${tweet.tweetText}</p>
+          <div class="tweet-details">
+            <span class="tweet-detail">
+              ${tweet.replies.length}
+            </span>
+            <span class="tweet-detail">
+              ${tweet.likes}
+            </span>
+            <span class="tweet-detail">
+              ${tweet.retweets}
+            </span>
+          </div>   
+        </div>            
+      </div>
+    </div>
+    `;
+  }
+  return feedHtml;
+}
+
+getFeedHtml();
