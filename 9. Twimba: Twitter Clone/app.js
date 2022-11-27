4733a75a06aef8acad53e517025700a6714d0c38
midCenturyDude7 <mgriffes@gmail.com>
2022-11-27 11:40:50 -0800

    Able to push new tweet to data.js and render it in html feed

--- 9. Twimba: Twitter Clone/app.js
+++ 9. Twimba: Twitter Clone/app.js
@@ -1,4 +1,5 @@
 import { tweetsData } from "./data.js";
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
 const tweetInput = document.getElementById("tweet-input");
 
@@ -47,7 +48,19 @@ function handleReplyClick(replyId) {
 }
 
 function handleTweetBtnClick() {
-  console.log(tweetInput.value);
+  tweetsData.unshift({
+    handle: "@Scimba",
+    profilePic: "images/troll.jpg",
+    likes: 0,
+    retweets: 0,
+    tweetText: tweetInput.value,
+    replies: [],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: uuidv4(),
+  });
+
+  render();
 }
 
 function getFeedHtml() {
